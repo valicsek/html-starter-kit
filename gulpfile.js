44a1bb3e9643df9a45d924c94ac970504c89834f
Valicsek David <entriek@gmail.com>
2018-07-18 15:25:54 +0200

    Fix that gulpfile uglify ES6 javascript instead of ES5

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 const gulp = require('gulp')
 
 const browserSync = require('browser-sync').create()
-const uglify = require('gulp-uglify')
+const uglifyes = require('gulp-uglify-es').default
 const uglifycss = require('gulp-uglifycss')
 const minifycss = require('gulp-clean-css')
 const imagemin = require('gulp-imagemin')
@@ -27,13 +27,14 @@ gulp.task('styles', () => {
   return gulp.src(path.join(__dirname, config.dev.source, '**', `*.${config.dev.style.file_type}`))
     .pipe(concat('all.css'))
     .pipe(minifycss())
+    .pipe(uglifycss())
     .pipe(gulp.dest(path.join(__dirname, config.build.source, 'assets', 'css')))
 })
 
 gulp.task('scripts', () => {
   return gulp.src(path.join(__dirname, config.dev.source, '**', `*.js`))
     .pipe(concat('all.js'))
-    .pipe(uglify())
+    .pipe(uglifyes())
     .pipe(gulp.dest(path.join(__dirname, config.build.source, 'assets', 'js')))
 })
 
